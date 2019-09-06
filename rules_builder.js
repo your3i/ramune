5769da4e89417616e53116478c1857891a37d9bf
nono <nono@pixiv.com>
2019-09-06 15:17:15 +0900

    json to yamlの変換ライブラリーを変える

--- rules_builder.js
+++ rules_builder.js
@@ -1,6 +1,6 @@
 const gsjson = require('google-spreadsheet-to-json');
 const GoogleSpreadsheet = require('google-spreadsheet');
-const yaml = require('js-yaml');
+const YAML = require('json-to-pretty-yaml');
 const fs = require('fs');
 
 const spreadsheet = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID);
@@ -60,8 +60,8 @@ function buildYmlRuleFile(data, output) {
         };
         rules.push(rule);
     }
-
-    const content = yaml.safeDump({
+    
+    const content = YAML.stringify({
         "imports": [],
         "rules": rules
     });
@@ -78,7 +78,7 @@ function buildYmlRuleFile(data, output) {
 };
 
 function importRuleFiles(ymls) {
-    var content = yaml.safeDump({
+    var content = YAML.stringify({
         "version": 1,
         "imports": ymls
     });
